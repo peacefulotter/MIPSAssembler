49b6eb2f2dd878541243fbb1a36193e917f62e7f
PeacefulOtter <zankro67@gmail.com>
2022-04-28 19:00:46 +0200

    Fixed wrong parsing of memory instructions

--- src/main/scala/parser/MParser.scala
+++ src/main/scala/parser/MParser.scala
@@ -8,8 +8,8 @@ import types.{Binary, Immediate, Register}
 object MParser extends Parser {
 
     override def parse(code: String, args: List[String], branches: Map[String, Int]): String = args match {
-        case (rt: String) :: (imm: String) :: _ =>
-            IParser.format(code, Register.parse(rt), Binary.zero, Immediate.parse(imm))
+        case (rt: String) :: (rs: String) :: (imm: String) :: _ =>
+            IParser.format(code, Register.parse(rt), Register.parse(rs), Immediate.parse(imm))
         case _ => throw new Exception(code + " " + args + " does not match for parsing Mem Instruction")
     }
 }
--- src/test/scala/AssemblerTest.scala
+++ src/test/scala/AssemblerTest.scala
@@ -13,6 +13,15 @@ class AssemblerTest extends AnyFunSuite {
         println(hex)
     }
 
+    test("assembler lw sw") {
+        val lines = List("nop", "addi t0 zero 0xFF", "sw t0 zero 0x8", "lw t0 zero 0x8")
+        println(lines)
+        val bin = Assembler.parseToBin(lines)
+        println(bin)
+        val hex = Assembler.parseToHex(lines)
+        println(hex)
+    }
+
     test("assembler with file") {
         Assembler.parseFile("test.txt", "output.txt")
     }
